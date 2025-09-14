60e8e6cffd4e0af19e6a0668eefa735286533e62
Ali <alialbaghdadi6@gmail.com>
2025-09-13 23:30:42 -0500

    more progress on pin cfg

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -3,35 +3,46 @@
 
 #define LED_PWM_FREQ_HZ 1000
 
-// colorTable[i][0:3] represent R, G, B, W channels of one color setting
+// colorTable[i][0:3] represent B, G, R, W channels of one color setting
 uint16_t colorTable[][4] = {
-	{0x1ff,	0x0,	0x0,	0x0},
-	{0x0,	0x1ff,	0x0,	0x0},
-	{0x0,	0x0,	0x1ff,	0x0},
-	{0x0,	0x0,	0x0,	0x1ff},
+	{0x0,	0x0,	0x1ff,	0x0},   // Color red
+    {0x0,	0x1ff,	0x1ff,	0x0},   // Color yellow
+	{0x0,	0x1ff,	0x0,	0x0},   // Color green
+    {0x1ff,	0x1ff,	0x0,	0x0},   // Color cyan
+	{0x1ff,	0x0,	0x0,	0x0},   // Color blue
+    {0x1ff,	0x0,	0x1ff,	0x0},   // Color magenta
+	{0x0,	0x0,	0x0,	0x1ff}, // Color white
 
 };
 
-int colorIndex = 0;
-
+// The color the penlight should display if it was on (index into colorTable)
+static volatile int currColor = 0;
 
 int main(void) {
     SystemInit();
 
     // RCC clock enable to all necessary units
     funGpioInitAll(); // inits AFIO + ports A C D
-    RCC->APB2PCENR |= (RCC_APB2Periph_TIM1 
-                    | RCC_APB2Periph_ADC1); // TIM1, ADC1 enable
+    RCC->APB2PCENR |= (RCC_APB2Periph_TIM1 |
+                        RCC_APB2Periph_ADC1); // TIM1, ADC1 enable
     
-    // Assign pin directions
-    funPinMode( PA1, GPIO_CFGLR_IN_PUPD ); // batt CHG_STAT
-    funPinMode( PD6, GPIO_CFGLR_IN_PUPD ); // SW1
-    funPinMode( PD5, GPIO_CFGLR_IN_PUPD ); // SW2
-    funPinMode( PA2, GPIO_CFGLR_IN_ANALOG ); // vbatt adc, on AIN0
-    // FIXME: to use AF or not to use AF?
-    funPinMode( PC3, GPIO_CFGLR_OUT_2Mhz_AF_PP ); // VREG_EN
+    // Assign directions to general-purpose pins
+    funPinMode(PA1, GPIO_CFGLR_IN_PUPD); // batt CHG_STAT
+    funPinMode(PA2, GPIO_CFGLR_IN_ANALOG); // vbatt adc, on AIN0
+    funPinMode(PC3, GPIO_CFGLR_OUT_2Mhz_PP); // VREG_EN
+    funPinMode(PD6, GPIO_CFGLR_IN_PUPD); // SW1
+    funPinMode(PD5, GPIO_CFGLR_IN_PUPD); // SW2
+    // Explicitly enable input pullup resistors for SWy by writing to GPIOx_OUTDR
+    funDigitalWrite(PD6, FUN_HIGH);
+    funDigitalWrite(PD5, FUN_HIGH);
+    
+    // Map and AF-configure pins to be used by TIM1
+    // TIM1 map: CH1=PC4(blue), CH2=PC7(green), CH3=PC5(red), CH4=PD4(white)
     AFIO->PCFR1 |= AFIO_PCFR1_TIM1_REMAP_FULLREMAP;
-    // TIM1 CH1=PC4(blue), CH2=PC7(green), CH3=PC5(red), CH4=PD4(white)
+    funPinMode(PC4, GPIO_CFGLR_OUT_2Mhz_AF_PP); // blue LED
+    funPinMode(PC7, GPIO_CFGLR_OUT_2Mhz_AF_PP); // green LED
+    funPinMode(PC5, GPIO_CFGLR_OUT_2Mhz_AF_PP); // red LED
+    funPinMode(PD4, GPIO_CFGLR_OUT_2Mhz_AF_PP); // white LED
 
     // Reset ADC1 to init all regs
     RCC->APB2PRSTR |= RCC_ADC1RST;
@@ -41,20 +52,25 @@ int main(void) {
 	RCC->APB2PRSTR |= RCC_TIM1RST;
 	RCC->APB2PRSTR &= ~RCC_TIM1RST;
 
-    // Set up timer regs to display current color
-    //colorIndex = 0;
+    // Set up timer duty cycles to display current color
     for (int i = 0; i < 4; ++i) {
-        // write colorTable[colorIndex][i] to TIM1 duty cycle
+        // write colorTable[currColor][i] to TIM1_CHi duty cycle reg
     }
 
+    // Enable power supply by toggling VREG_EN, thus turning on the LED
+    funDigitalWrite(PC3, FUN_HIGH);
+
     // Enable button interrupts
+
+    // ADC reads battery periodically using system periodic wake timer
+    // int val = funAnalogRead(0);
 
     // Enter sleep
     __WFI();
 
     /*
      * At this point the light is active and the mcu can sleep. The button interrupts
-     * wake the mcu from WFI, and their ISRs simply inc/dec colorIndex, send the
+     * wake the mcu from WFI, and their ISRs simply inc/dec currColor, send the
      * new color to TIM1, and reenter sleep. If the button is long-pressed (user
      * wants to turn off the device), it should disable the light and the boost conv,
      * then WFE (standby).
